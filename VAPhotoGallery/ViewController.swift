74d35efeab66bcc7c546e6360bdb380dc1e17565
Vikash Anand <anandin02@gmail.com>
2020-04-26 23:53:36 +0530

    Initial Commit

---
+++ VAPhotoGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VAPhotoGallery
+//
+//  Created by Vikash Anand on 26/04/20.
+//  Copyright © 2020 Vikash Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
